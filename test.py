079e3707b80a5cc46f001c34a4e593f8442b01e7
shriyaY <111619499+shriyaY@users.noreply.github.com>
2023-11-02 18:36:03 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+multiples = [5, 25, 35, 45, 55]
+a = [0, 90]
+multiples.extend(a)
+# print(multiples)
+# print(len(multiples))
+
+
+n = {10, 90, 20, 102, 10, 90, 67, 55, 277, 90} #SET
+# print(n)
+
+b = multiples.index(35)
+# print(b)
